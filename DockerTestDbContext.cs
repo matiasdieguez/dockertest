d61564ba184f3340434ecb20a534dedb25416b04
Matías Dieguez <matiasdieguez@hotmail.com>
2022-10-25 15:07:56 -0300

    initial

---
+++ DockerTestDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+public class DockerTestDbContext : DbContext
+{
+    public DbSet<Dummy> Dummies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseOracle(@"Data Source=localhost:1521/XE;User Id=C##DOCKERTEST;Password=Tecnocode2022$");
+    }
+}
+
+public class Dummy
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var dbContext = new DockerTestDbContext();
+dbContext.Dummies.Add(new Dummy { Name = Guid.NewGuid().ToString() });
+dbContext.SaveChanges();
+
+foreach (var dummy in dbContext.Dummies.ToList())
+{
+    Console.WriteLine($"Dummy {dummy.Name}");
+}
+
+Console.WriteLine("Bye!");
